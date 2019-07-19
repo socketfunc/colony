69386c4205af9e8b3ccfce89d34a9464b5ff526e
yu-icchi <yu.ichiko@gmail.com>
2019-07-20 01:41:50 +0900

    feat(pkg): add jwt

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ proto-fmt:
 	find ./proto/ -name "*.proto" | xargs clang-format -i
 
 gazelle:
+	bazel run //:gazelle
+
+update-repos:
 	bazel run //:gazelle -- update-repos -from_file=go.mod
 
 test:
---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,33 @@
+package jwt
+
+import (
+	"github.com/gbrlsnchs/jwt/v3"
+)
+
+type Payload struct {
+	jwt.Payload
+	Scopes []string
+}
+
+type JWT interface {
+	Sign(payload *Payload) (string, error)
+	Verify(token string) (*Payload, error)
+}
+
+type auth struct {
+	secret jwt.Algorithm
+}
+
+func (a *auth) Sign(payload *Payload) (string, error) {
+	token, err := jwt.Sign(payload, a.secret)
+	if err != nil {
+		return "", err
+	}
+	return string(token), nil
+}
+
+func (a *auth) Verify(token string) (*Payload, error) {
+	pl := new(Payload)
+	_, err := jwt.Verify([]byte(token), a.secret, pl)
+	return pl, err
+}
